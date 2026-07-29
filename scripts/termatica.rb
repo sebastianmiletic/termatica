7864e556a345ec47a59f7fa4273153210b1b7775
dominicmods <sebastian.miletic043@gmail.com>
2026-07-29 19:39:07 +1000

    Update 1.2.0 release artifact checksum

--- scripts/termatica.rb
+++ scripts/termatica.rb
@@ -1,6 +1,6 @@
 cask "termatica" do
   version "1.2.0"
-  sha256 "fe7a3caaf68fb3f5b34c41ef613d17963ffa8cb05102d86a778a1b04276248c1"
+  sha256 "3ace3ad8f7f709bfb2207137f2748ec3a632a977a5864f5a8c18ab59702120e6"
 
   url "https://github.com/sebastianmiletic/termatica/releases/download/v#{version}/Termatica-macOS-universal.dmg"
   name "Termatica"
